2ac2b01c727c1cb05a201f67147263b3b41c5c79
FadhlanAnfasal <fadhlaanalle149@gmail.com>
2025-08-06 06:59:01 +0700

    update apa aja

--- umkm.js
+++ umkm.js
@@ -3,7 +3,7 @@ const dataUMKM = [
     nama: "Toko Dara",
     jam: "05:30 - 21:30",
     jenis: "Sembako",
-    alamat: "548P, RT07/RW.01, Dukuh Manding, Kepohkencono",
+    alamat: "548P+284, RT07/RW.01, Dukuh Manding, Kepohkencono",
     kontak: "081252708714"
   },
   {
@@ -12,6 +12,34 @@ const dataUMKM = [
     jenis: "Warung Kopi",
     alamat: "547M+JP6, RT07/RW.01, Dukuh Manding, Kepohkencono",
     kontak: "085321054292"
+  },
+  {
+    nama: "Agen Ibu Sutikjo",
+    jam: "05:30 - 21:30",
+    jenis: "Agen Sembako",
+    alamat: "547M+JP6, RT07/RW.01, Dukuh Manding, Kepohkencono",
+    kontak: "081392429129"
+  },
+  {
+    nama: "Toko Sinar Tani",
+    jam: "06:00 - 17:00",
+    jenis: "Toko Pertanian",
+    alamat: "547M+JP6, RT07/RW.01, Dukuh Manding, Kepohkencono",
+    kontak: "081225344504"
+  },
+  {
+    nama: "Toko Nurhayati",
+    jam: "06:00 - 21:00",
+    jenis: "Sembako dan Agen Token",
+    alamat: "547M+JP6, RT07/RW.01, Dukuh Manding, Kepohkencono",
+    kontak: "085290653745"
+  },
+  {
+    nama: "Toko Tani Makmur",
+    jam: "06:00 - 17:00 (Tentatif)",
+    jenis: "Toko Pertanian",
+    alamat: "547M+JP6, RT07/RW.01, Dukuh Manding, Kepohkencono",
+    kontak: "085325336743"
   }
 ];
 
